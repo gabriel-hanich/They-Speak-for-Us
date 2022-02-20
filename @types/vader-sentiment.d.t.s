605cb07e58a4b874a2dfe6b43d8ac1ae552b9cd1
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-02-20 19:15:50 +1100

    Connected to mongo db

---
+++ @types/vader-sentiment.d.t.s
@@ -0,0 +1,4 @@
+declare module 'vader-sentiment' {
+    var vader-sentiment: any; // probably want to make this more specific
+    export = vader-sentiment;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello from ts")
+/// <reference path="../@types/vader-sentiment.d.t.s" />
+import vaderSentiment from "vader-sentiment";
+import csvParser from "csv-parser";
+import fs from "fs";
+
+fs.createReadStream('/data/Media Outlets.csv')
+  .pipe(csvParser())
+  .on('data', (row) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
