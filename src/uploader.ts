e06782d78473eaffb99828d55005d28da31e106d
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-02-23 17:13:42 +1100

    Finished Uploader.ts

--- src/uploader.ts
+++ src/uploader.ts
@@ -6,16 +6,18 @@ import path, { resolve } from "path";
 import dotenv from "dotenv";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
+// Constants
 const dataVersion: number = 2;
 dotenv.config({path: path.resolve(__dirname, '../.env')});
 
 interface article{
-  isLegacy: boolean,
+  outletName: string
   headline:string,
   description: string,
   author: string,
   publishDate: Date,
-  sentimentScore?: number,
+  isLegacy: boolean,
+  sentimentScore: number,
   // Modern article data points below, legacy and modern data points above
   linkToArticle?: string,
   imageURL?: string
@@ -32,69 +34,69 @@ interface mediaOutlet{
 
 const getOutletList = async (): Promise<mediaOutlet[]> => { // Gets list containing all media outlets present in the 'Media Outlets.csv' File
   return new Promise((resolve, reject)=>{ 
-    var outletList: mediaOutlet[] = [];
+    var outletList: mediaOutlet[] = []; // Array containing all present media outlets
     fs.createReadStream(path.resolve(__dirname, `../data/${dataVersion}/Media Outlets.csv`))
     .pipe(CsvParser())
     .on('data', function(data){
-        try {
-            outletList.push({name: data.outletName, rssLink: data.RSSLink, articleList: []})
-        }
-        catch(err) {
-            //error handler
-        }
+        outletList.push({name: data.outletName, rssLink: data.RSSLink, articleList: []}); // Add each line to the array 
     })
     .on('end',function(){
-        resolve(outletList)
+        resolve(outletList);
     });  
   })
 }
 
 const getArticles = async (outletName:string): Promise<article[]> => { // Get all the articles present for an outlet and compute the sentiment score
   return new Promise((resolve, reject)=>{
-    var articleList: article[] = [];
-    if(fs.existsSync(path.resolve(__dirname, `../data/${dataVersion}/csv/${outletName}.csv`))){
+    var articleList: article[] = []; // Array containig each article for specific outlet
+    if(fs.existsSync(path.resolve(__dirname, `../data/${dataVersion}/csv/${outletName}.csv`))){ // Ensure outlets file exists
       fs.createReadStream(path.resolve(__dirname, `../data/${dataVersion}/csv/${outletName}.csv`), "utf-8")
       .pipe(CsvParser())
       .on('data', function(data){
-          try {
-            var thisSentimentScore: number =vaderSentiment.SentimentIntensityAnalyzer.polarity_scores(data.headline)["compound"]; 
-            articleList.push({isLegacy: true, 
-                              headline: data.headline, 
-                              description: data.description,
-                              author: data.author,
-                              publishDate: new Date(data.date),
-                              sentimentScore: thisSentimentScore})
-          }
-          catch(err) {
-              //error handler
-          }
+          var thisSentimentScore: number =vaderSentiment.SentimentIntensityAnalyzer.polarity_scores(data.headline)["compound"]; // Generate sentiment score
+          articleList.push({outletName: outletName,
+                            isLegacy: true, 
+                            headline: data.headline, 
+                            description: data.description,
+                            author: data.author,
+                            publishDate: new Date(data.date),
+                            sentimentScore: thisSentimentScore});
     })
     .on('end', ()=>{
-      resolve(articleList);
+      resolve(articleList); // Return array
     })
-    }else{
-      reject(`File Not Found for outlet ${outletName}`)
+    }else{ // If file is not found for specific outlet
+      console.log(`File Not Found for outlet ${outletName}`);
+      reject(`File Not Found for outlet ${outletName}`);
     }
   });
 }
 
-(async () => {
-  const dbClient = new MongoClient(process.env.DB_URI as string)
+(async () => { // Actually run some code
+  const dbClient = new MongoClient(process.env.DB_URI as string);
 
+  // Get array containing each tracked outlet
   const outletList: mediaOutlet[] = await getOutletList();
+  // Get array with each article for each outlet
   for(var i:number=0; i<outletList.length; i++){
     outletList[i]["articleList"] = await getArticles(outletList[i]["name"])
     console.log(`${i+ 1}/${outletList.length}`)
   }
+  // Upload data to db
   console.log("CONNECTING TO DATABASE");
   dbClient.connect(err =>{
+    const articleCollection = dbClient.db("test").collection("newsData");
+    const outletCollection = dbClient.db("test").collection("outletsList");
+    var articleCount: number = 0; // Track how many articles are uploaded
     for(var i:number=0; i<outletList.length; i++){
       console.log(`Starting ${i + 1} of ${outletList.length}`)
-      const dbCollection = dbClient.db("test").collection(outletList[i]["name"]);
+      outletCollection.insertOne({"outletName": outletList[i]["name"], "rssLink": outletList[i]["rssLink"]})
       for(var aI: number = 0; aI<outletList[i]["articleList"].length; aI++){
-        var a = dbCollection.insertOne(outletList[i]["articleList"][aI])
+        var a = articleCollection.insertOne(outletList[i]["articleList"][aI])
+        articleCount += 1;
       }
     }
+    console.log(`Began uploading ${articleCount} articles`)
   })
 })();
 
