f52a7cc4eb5ad4c403143aae07c323ca47bcb398
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-09-07 18:16:25 +1000

    Added text print-outs

---
+++ Tooling/src/media.py
@@ -0,0 +1,16 @@
+class Outlet:
+    def __init__(self, name):
+        self.name = name
+        self.articleList = []
+
+    def addArticle(self, article) -> None:
+        self.articleList.append(article)
+
+class Article:
+    def __init__(self, outlet, headline, description, author, date, sentimentScore):
+        self.outlet = outlet
+        self.headline = headline
+        self.description = description
+        self.author = author
+        self.date = date
+        self.sentimentScore = sentimentScore
