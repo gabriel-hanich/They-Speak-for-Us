70bc967612d60203da132799382e71ef59d55df2
Gabriel Hanich <68582067+gabriel-hanich@users.noreply.github.com>
2022-11-19 01:14:21 +1100

    Finished home page

--- Web App/They-Speak-For-Us/src/app/components/pages/home-page/home-page.component.ts
+++ Web App/They-Speak-For-Us/src/app/components/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faNewspaper, faRadio } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-page',
@@ -9,9 +9,12 @@ import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
 export class HomePageComponent implements OnInit {
   public articleCount: number = 0;
   public publisherCount: number = 0;
+  public dayCount: number = 0;
 
   // Icons
   public icoNewspaper = faNewspaper;
+  public icoPublisher = faRadio;
+  public icoClock = faClock;
 
   constructor() { }
 
